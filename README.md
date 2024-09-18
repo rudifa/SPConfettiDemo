24e03020dc14197acbdea4037b5a813b8016da60
Rudi Farkas <rudi.farkas@gmail.com>
2024-09-18 22:52:05 +0200

    add SPConfetti and throw confetti in the app

---
+++ README.md
@@ -0,0 +1,5 @@
+#  SPConfettiDemo
+
+This app will demonstrate the use of the package [SPConfetti](https://github.com/ivanvorobei/SPConfetti).
+
+
--- SPConfettiDemo/ContentView.swift
+++ SPConfettiDemo/ContentView.swift
@@ -6,14 +6,30 @@
 //
 
 import SwiftUI
+import SPConfetti
 
 struct ContentView: View {
+    @State private var showConfetti = false
+
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Spacer()
+            Button(action: {
+                showConfetti.toggle()
+                if showConfetti {
+                    SPConfetti.startAnimating(.centerWidthToDown, particles: [.triangle, .star, .heart], duration: 2)
+                } else {
+                    SPConfetti.stopAnimating()
+                }
+            }) {
+                Text("Toggle Confetti")
+            }
+            Spacer()
         }
         .padding()
     }
