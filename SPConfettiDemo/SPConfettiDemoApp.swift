62465e8f7848dea52cffb65603efa8bbafa48c1c
Rudi Farkas <rudi.farkas@gmail.com>
2024-09-18 22:22:03 +0200

    Initial Commit

---
+++ SPConfettiDemo/SPConfettiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPConfettiDemoApp.swift
+//  SPConfettiDemo
+//
+//  Created by Rudolf Farkas on 18.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SPConfettiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
